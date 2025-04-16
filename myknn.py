5141bb2b7498e75147fd4e0215533a9cd9a7df40
Zharken Utegenov <72005018+Zhar04@users.noreply.github.com>
2025-04-16 17:50:12 +0600

    Create myknn.py

---
+++ myknn.py
@@ -0,0 +1,12 @@
+import numpy as np
+from scipy.stats import mode
+
+class KNN:
+    def __init__(self, n_neighbors=5, p=2):
+        # your code
+
+    def fit(self, X, y):
+        # your code
+        
+    def predict(self, X):
+        # you
